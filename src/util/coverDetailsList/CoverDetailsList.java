68905b302354414eba22594cfdd97b119466c4b0
Christian Rech <rech.christian@googlemail.com>
2010-09-14 11:29:27 +0200

    asdsd

--- src/util/coverDetailsList/CoverDetailsList.java
+++ src/util/coverDetailsList/CoverDetailsList.java
@@ -13,7 +13,10 @@ import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import muvibee.lists.BookList;
 import muvibee.lists.MediaList;
+import muvibee.lists.MusicList;
+import muvibee.lists.VideoList;
 
 import muvibee.media.*;
 
@@ -23,11 +26,11 @@ public class CoverDetailsList extends JPanel implements Observer{
 	CoverDetailsListRenderer lcr;
 	JList list;
 	
-	public CoverDetailsList() {
-			setLayout(new BorderLayout());
+	public CoverDetailsList(MediaList mediaList) {
+                setLayout(new BorderLayout());
 		
-			listModel = new DefaultListModel();
-			lcr = new CoverDetailsListRenderer();
+		listModel = new DefaultListModel();
+		lcr = new CoverDetailsListRenderer();
  	        list = new JList(listModel);
  	        list.setCellRenderer(lcr);
  	        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -36,7 +39,9 @@ public class CoverDetailsList extends JPanel implements Observer{
  	        
 	        list.addListSelectionListener(new ListSelectionListener() {
 	        	public void valueChanged(ListSelectionEvent evt){
-	        		System.out.println("Feld: " + list.getSelectedIndex() + " " + (evt.getValueIsAdjusting()?"PREP":"SEL"));
+                            if(evt.getValueIsAdjusting())
+                                mediaList.getList().get(list.getSelectedIndex());
+//	        		System.out.println("Feld: " + list.getSelectedIndex() + " " + (evt.getValueIsAdjusting()?"PREP":"SEL"));
 	        	}
 	        });
  	        
