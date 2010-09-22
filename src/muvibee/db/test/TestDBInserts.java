f3a652bb7457691b00bb9813be88d55fb7456d90
Christian Rech <rech.christian@googlemail.com>
2010-09-22 11:08:09 +0200

    Fehler in DBinsert gefixt

--- src/muvibee/db/test/TestDBInserts.java
+++ src/muvibee/db/test/TestDBInserts.java
@@ -11,6 +11,9 @@ package muvibee.db.test;
  */
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import muvibee.media.Book;
 import muvibee.media.Music;
@@ -20,7 +23,7 @@ import muvibee.ean.EanAmazon;
 public class TestDBInserts {
 
 	public static void main(String[] args) {
-
+        try {
             //Illuminati Buch EAN
             Book b1 = new Book();
             b1 = (Book) EanAmazon.searchEan("9783404148660");
@@ -41,42 +44,45 @@ public class TestDBInserts {
             Music m1 = new Music();
             m1 = (Music) EanAmazon.searchEan("0886974796626");
             m1.updateDB();
-//                Book b1 = new Book();
-//                // Illuminati
-//                b1 = (Book) EAN.searchEan("9783404148660");
-//                b1.updateDB();
-//		Book b = new Book();
-//                b.setID(3);
-//                b.setTitle("Buchtitel ID 3 UPDATED");
-//		BufferedImage bi1 = new BufferedImage(1, 2, 3);
-//                b.setCover(bi1);
-//		b.setLendDate("2010-11-10");
-//		b.setLendUntilDate("2010-10-10");
-//		System.out.println(b.toString());
-//		b.updateDB();
-//                System.out.println("----------------------------");
-//
-//		Music m = new Music();
-//                m.setID(1);
-//                m.setTitle("Musiktitel ID 1 Update");
-//                BufferedImage bi2 = new BufferedImage(4, 5, 6);
-//                m.setCover(bi2);
-//		m.setLendDate("2010-11-10");
-//		m.setLendUntilDate("2010-10-10");
-//		System.out.println(m.toString());
-//		m.updateDB();
-//                System.out.println("----------------------------");
-//
-//
-//		Video v = new Video();
-//                v.setTitle("Videotitel");
-//		BufferedImage bi3 = new BufferedImage(7, 8, 9);
-//		v.setCover(bi3);
-//		v.setLendDate("2010-11-10");
-//		v.setLendUntilDate("2010-10-10");
-//		System.out.println(v.toString());
-//		v.updateDB();
-//                System.out.println("----------------------------");
+            //                Book b1 = new Book();
+            //                // Illuminati
+            //                b1 = (Book) EAN.searchEan("9783404148660");
+            //                b1.updateDB();
+            //		Book b = new Book();
+            //                b.setID(3);
+            //                b.setTitle("Buchtitel ID 3 UPDATED");
+            //		BufferedImage bi1 = new BufferedImage(1, 2, 3);
+            //                b.setCover(bi1);
+            //		b.setLendDate("2010-11-10");
+            //		b.setLendUntilDate("2010-10-10");
+            //		System.out.println(b.toString());
+            //		b.updateDB();
+            //                System.out.println("----------------------------");
+            //
+            //		Music m = new Music();
+            //                m.setID(1);
+            //                m.setTitle("Musiktitel ID 1 Update");
+            //                BufferedImage bi2 = new BufferedImage(4, 5, 6);
+            //                m.setCover(bi2);
+            //		m.setLendDate("2010-11-10");
+            //		m.setLendUntilDate("2010-10-10");
+            //		System.out.println(m.toString());
+            //		m.updateDB();
+            //                System.out.println("----------------------------");
+            //
+            //
+            //		Video v = new Video();
+            //                v.setTitle("Videotitel");
+            //		BufferedImage bi3 = new BufferedImage(7, 8, 9);
+            //		v.setCover(bi3);
+            //		v.setLendDate("2010-11-10");
+            //		v.setLendUntilDate("2010-10-10");
+            //		System.out.println(v.toString());
+            //		v.updateDB();
+            //                System.out.println("----------------------------");
+        } catch (IOException ex) {
+            Logger.getLogger(TestDBInserts.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
 
 
